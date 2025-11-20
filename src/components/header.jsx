7e698153725f98eedcc6517776f3a71d2ad79f9a
fredyuruena <fredydurango@ene8.com.mx>
2025-11-20 17:56:04 -0500

    1er Commit

---
+++ src/components/header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function header() {
+  return (
+    <nav>
+        <div>
+            <h1>
+                
+            </h1>
+        </div>
+    </nav>
+  )
+}
+
+export default header
